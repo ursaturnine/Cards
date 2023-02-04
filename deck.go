8684f9fdf23ac619f62de2db4900e9fd526daae5
Tyrah <tyrah96@gmail.com>
2023-02-04 12:27:20 -0800

    modified shuffle function to achieve true randomization. demonstrating use of Go packages. time, rand and subpackage, UnixNano

--- deck.go
+++ deck.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck
@@ -80,8 +81,11 @@ func newDeckFromFile(filename string) deck {
 // Shuffle function
 // uses random library to swap index positions
 func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
 	for i := range d {
-		newPosition := rand.Intn(len(d) - 1)
+		newPosition := r.Intn(len(d) - 1)
 
 		d[i], d[newPosition] = d[newPosition], d[i]
 	}
